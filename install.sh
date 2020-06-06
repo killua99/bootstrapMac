1378b9fc1cc8370fc1ca538f7a817fcd93a3ecac
Luigi Guevara <luigi.fox15@gmail.com>
2020-06-06 22:39:02 +0200

    WIP

--- install.sh
+++ install.sh
@@ -82,8 +82,8 @@ fi
 [ -f "/usr/local/var/homebrew" ] && sudo chmod -R 775 /usr/local/var/homebrew
 [ -f "/usr/local/Cellar" ] && sudo chmod -R 775 /usr/local/Cellar
 
-[ -x "$(which brew)" ] && /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+[ ! -x "$(which brew)" ] && /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
-[ -x "$(which ansible-playbook)" ] && brew install ansible
+[ ! -x "$(which ansible-playbook)" ] && brew install ansible
 
 ansible-playbook bootstrap.yml
